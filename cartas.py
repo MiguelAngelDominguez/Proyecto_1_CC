7dc5abc0c319aa9b4e4baab2ed54e1743f8ab87e
rbtracy <tcruniversity@gmail.com>
2025-12-02 09:31:47 -0500

    update carta 12

--- cartas.py
+++ cartas.py
@@ -170,8 +170,7 @@ def aplicar_carta(numero, estado):
     # Duración: 2 turnos
     elif numero == 12:
         estado["ReductorBoicot"] = 0.5
-        if estado["TurnosBoicot"] <= 1:
-            estado["TurnosBoicot"] = 2
+        estado["TurnosBoicot"] = 2
         return estado
 
     # Carta 13: Error de etiquetado
